cfd65021d925c2aed03c093da1c4411e1559673d
unknown <karansinghpargai7@gmail.com>
2022-02-21 18:26:44 +0530

    First Git

---
+++ android/app/src/main/kotlin/com/android/animetion/animetion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.android.animetion.animetion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Animetion());
+}
+
+class Animetion extends StatefulWidget {
+  @override
+  _AnimetionState createState() => _AnimetionState();
+}
+
+class _AnimetionState extends State<Animetion> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Card(
+          ),
+        ],
+      ),
+    );
+  }
+}
