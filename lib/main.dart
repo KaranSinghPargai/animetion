b69cd84096fcbc70cf25b05b52f6115b7632d75e
KaranSinghPargai <karansinghpargai7@gmail.com>
2022-02-25 17:48:58 +0530

    data fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+const String jikanApiURL = 'https://api.jikan.moe/v4';
 void main() {
   runApp(Animetion());
 }
@@ -10,12 +13,37 @@ class Animetion extends StatefulWidget {
 }
 
 class _AnimetionState extends State<Animetion> {
+  List listResponse=[];
+  Future jikanApiCall() async {
+    http.Response apiResponse;
+    apiResponse = await http.get(Uri.parse('$jikanApiURL/top/characters'));
+    print(apiResponse);
+    if (apiResponse.statusCode == 200) {
+      setState(() {
+        listResponse = json.decode(apiResponse.body);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    jikanApiCall();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-      ),
+    return Scaffold(
+        body: ListView.builder(itemBuilder: (context, index) {
+          return Container(
+            child: Row(
+              children: [
+                Image.network(listResponse['data'][index]['image']['image_url']),
+              ],
+            ),
+          );
+        }
+        itemCount:listResponse.isNotEmpty? listResponse.length: 0,
+        ),
     );
   }
-}
